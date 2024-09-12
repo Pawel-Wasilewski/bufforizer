f49be5b97fd19672b0285934202d7c4d3a522008
Pawel-Wasilewski <wasilewski.pawel2007@gmail.com>
2024-09-12 21:24:47 +0200

    0.0.0-4 Alpha - Rozpoczęcie pisania funkcjonalności zczytywania plików

--- src/Gui.java
+++ src/Gui.java
@@ -1,8 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
 
-public class Gui {
+public class Gui extends JFrame {
     JFrame gui;
+    JPanel selectedimages_gui;
     Gui() {
 
         /*
@@ -15,7 +21,7 @@ public class Gui {
         gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gui.setSize(800,500);
         gui.setLocationRelativeTo(null);
-        gui.setLayout(new FlowLayout());
+        gui.setLayout(new BorderLayout());
         gui.setVisible(true);
         gui.setResizable(false);
 
@@ -28,7 +34,50 @@ public class Gui {
         /*
             COMPONENTS
          */
+
+            // Text inputs
+
+        JLabel title_gui = new JLabel("Bufforizer");
+        JLabel desc_gui = new JLabel("Automatycznie zmienia rozmiar, dodaje znak wodny, zmienia nazwe pliku, i dodaje na WP oraz na jego DB");
+        JLabel warning_gui = new JLabel("WARNING: Oprogramowanie jest jeszcze w niedziałającym stanie. Nie zalecane jest używanie.");
+        JLabel author_gui = new JLabel("Autor: Paweł Wasilewski");
+
+            // User interactive components
+
+        JButton fileinsert_gui = new JButton("Dodaj pliki");
+        JButton startformatting_gui = new JButton("Zacznij Formatować");
+
+        /*
+            EVENT FUNCTIONALITY
+         */
+
+            // File inserter
+
+        fileinsert_gui.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileinserter = new JFileChooser();
+
+                // Adding multi-filing option
+
+                fileinserter.setMultiSelectionEnabled(true);
+
+                int result = fileinserter.showOpenDialog(null);
+
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    File[] selectedfiles = fileinserter.getSelectedFiles();
+                    selectedimages_gui.removeAll();
+
+                    try (FileWriter writer = new FileWriter("")){
+
+                    }
+                    for (File file: selectedfiles) {
+                    }
+                }
+            }
+        });
     }
+
 
 
 
